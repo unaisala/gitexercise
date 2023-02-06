450b5333dd6f07fe5c73bedf683b3471499a2dd7
Unai <salaunai9@gmail.com>
2023-02-06 15:20:01 +0100

    Add README file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Software Engineering 2022-2023
+
+An exercise for practicing with GitHub and IntelliJ.
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.example.gitexercise {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.gitexercise to javafx.fxml;
     exports com.example.gitexercise;
 }
